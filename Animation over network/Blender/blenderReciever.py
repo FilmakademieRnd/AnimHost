64416be59c1b049f5459e7fa82e69ef34b819452
Alexandru Schwartz <alexandru.schwartz@filmakademie.de>
2023-03-29 13:50:52 +0200

    scripts modifications
    fully commented both scripts and we are also sending the transform.position of the root.

--- Animation over network/Blender/blenderReciever.py
+++ Animation over network/Blender/blenderReciever.py
@@ -4,6 +4,7 @@ import threading
 import mathutils
 import math
 
+# Define a dictionary that maps Unity bone names to Mixamo bone names
 unity_to_blender_bone_mapping = {
     # //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-BODY+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
     # //body/body
@@ -84,65 +85,77 @@ unity_to_blender_bone_mapping = {
 
 
 
+# Define function to decode quaternion from UTF-8 encoded byte string
 def decode_quaternion(utf8_bytes):
-    vector_string = utf8_bytes.decode('utf-8')
-    vector_string = vector_string.strip('()')  # Remove the parentheses
-    x, y, z, w = map(float, vector_string.split(','))
-    return mathutils.Quaternion((w, x, y, z))
+    vector_string = utf8_bytes.decode('utf-8')  # Decode byte string to UTF-8 string
+    vector_string = vector_string.strip('()')  # Remove parentheses from string
+    x, y, z, w = map(float, vector_string.split(','))  # Split string into float values and store in variables
+    return mathutils.Quaternion((w, x, y, z))  # Return Quaternion object created from float values
+
+# Define function to decode vector from UTF-8 encoded byte string
+def decode_transform(utf8_bytes):
+    vector_string = utf8_bytes.decode('utf-8')  # Decode byte string to UTF-8 string
+    vector_string = vector_string.strip('()')  # Remove parentheses from string
+    x, y, z = map(float, vector_string.split(','))  # Split string into float values and store in variables
+    return mathutils.Vector((x, y, z))  # Return Vector object created from float values
 
 # Set the IP address and port number from the publisher script
 ip = "127.0.0.1"
 port = "5555"
-context = zmq.Context()
+context = zmq.Context()  # Create ZeroMQ context object
 
-
+# Define function to receive and process data from publisher
 def blender_receiver():
-    socket = context.socket(zmq.SUB)
-    socket.setsockopt_string(zmq.SUBSCRIBE, "foo")
-    for unity_bone_name in unity_to_blender_bone_mapping:
+    socket = context.socket(zmq.SUB)  # Create ZeroMQ subscriber socket
+    socket.setsockopt_string(zmq.SUBSCRIBE, "foo")  # Subscribe to "foo" topic
+    socket.setsockopt_string(zmq.SUBSCRIBE, "transform")  # Subscribe to "transform" topic
+    for unity_bone_name in unity_to_blender_bone_mapping:  # Subscribe to bone names specified in unity_to_blender_bone_mapping
         socket.setsockopt_string(zmq.SUBSCRIBE, unity_bone_name)
 
-    socket.bind("tcp://{}:{}".format(ip, port))
+    socket.bind("tcp://{}:{}".format(ip, port))  # Bind socket to specified IP address and port number
 
     print("Subscriber connected to: tcp://{}:{}\nWaiting for data...".format(ip, port))
 
     # Continuously receive and process data from the publisher
     while True:
-        topic, message = socket.recv_multipart()
-        # print("Received message on topic {}: {}".format(topic, message))
+        topic, message = socket.recv_multipart()  # Receive data as a topic and message pair
 
-        # Decode the received quaternion message
-        quaternion = decode_quaternion(message)
-        newQuatRot = mathutils.Quaternion((quaternion.w, quaternion.x, -quaternion.y, -quaternion.z))
+        if topic == b"transform":  # If the topic is "transform"
+            transform = decode_transform(message)  # Decode the message into a vector
+            # Set the location of the "Armature" object in Blender based on the received vector
+            bpy.data.objects['Armature'].location.x = -transform.x
+            bpy.data.objects['Armature'].location.y = -transform.z
+            bpy.data.objects['Armature'].location.z = transform.y
 
-        # ROOT
+        else:
+            # If the topic is not "transform", decode the message into a quaternion
+            quaternion = decode_quaternion(message)
+            # Create a new quaternion with different axis order and orientation to match Blender's coordinate system
+            newQuatRot = mathutils.Quaternion((quaternion.w, quaternion.x, -quaternion.y, -quaternion.z))
+
+        # If the topic is "foo"
         if topic == b"foo":
-            # ADD math.radians(90) on the x axis
-            bpy.data.objects['Armature'].rotation_mode = 'QUATERNION'
-            bpy.data.objects['Armature'].rotation_quaternion = newQuatRot
-            bpy.data.objects['Armature'].rotation_mode = 'XYZ'
-            bpy.data.objects['Armature'].rotation_euler.rotate_axis("X", math.radians(90))
+            # Create a new quaternion with different axis order and orientation to match Blender's coordinate system
+            newQuatRot = mathutils.Quaternion((quaternion.w, quaternion.x, -quaternion.z, -quaternion.y))
+            bpy.data.objects['Armature'].rotation_mode = 'QUATERNION'  # Set rotation mode to quaternion
+            bpy.data.objects['Armature'].rotation_quaternion = newQuatRot  # Set the rotation of the "Armature" object in Blender to the new quaternion
+            bpy.data.objects['Armature'].rotation_mode = 'XYZ'  # Set rotation mode to Euler angles
+            bpy.data.objects['Armature'].rotation_euler.rotate_axis("X", math.radians(90))  # Rotate 90 degrees around the X axis
 
-        # BONES
+        # If the topic is a bone name specified in unity_to_blender_bone_mapping
         bone_name = topic.decode('utf-8')  # Convert the topic byte string to a regular string
         if bone_name in unity_to_blender_bone_mapping:
-            blender_bone_name = unity_to_blender_bone_mapping[bone_name]
+            blender_bone_name = unity_to_blender_bone_mapping[bone_name]  # Get the corresponding Blender bone name from unity_to_blender_bone_mapping
 
             # Apply the received rotation to the corresponding bone in Blender
-            armature = bpy.data.objects['Armature']
-            if blender_bone_name in armature.pose.bones:
-                blender_bone = armature.pose.bones[blender_bone_name]
+            armature = bpy.data.objects['Armature']  # Get the "Armature" object in Blender
+            if blender_bone_name in armature.pose.bones:  # Check if the corresponding bone exists in the armature
+                blender_bone = armature.pose.bones[blender_bone_name]  # Get the corresponding bone from the armature
 
-
-                blender_bone.rotation_mode = 'XYZ'
-
-                blender_bone.rotation_euler = newQuatRot.to_euler()
-
-                blender_bone.rotation_mode = 'XYZ'
-
-
+                blender_bone.rotation_mode = 'QUATERNION'  # Set rotation mode to quaternion
+                blender_bone.rotation_quaternion = newQuatRot  # Set the rotation of the bone to the new quaternion
 
 # Start the receiver in a separate thread
-receiver_thread = threading.Thread(target=blender_receiver)
-receiver_thread.daemon = True
-receiver_thread.start()
+receiver_thread = threading.Thread(target=blender_receiver)  # Create a new thread that runs the blender_receiver function
+receiver_thread.daemon = True  # Set the thread to run as a daemon, so that it will automatically stop when the main program stops
+receiver_thread.start()  # Start the thread
--- Animation over network/Unity/Publisher.cs
+++ Animation over network/Unity/Publisher.cs
@@ -1,149 +1,185 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using NetMQ;
 using NetMQ.Sockets;
 using System.Text;
-using System.Threading;
-using UnityEngine.Serialization;
 
 public class Publisher : MonoBehaviour
 {
     // IP address and port to connect to
     public string ip = "127.0.0.1";
     public string port = "5555";
-    
+
     private PublisherSocket socket;
     private string topic = "foo";
     private string topicBones = "bones";
     private int i = 0;
     private bool on = true;
     private Quaternion previousRotation;
+    private Vector3 previousPos;
     private Quaternion blenderInitialRotation;
+    private Quaternion armatureOffset;
+
     Animator animator;
 
     [SerializeField] private GameObject Model;
+    [SerializeField] private GameObject Hips;
 
     public List<Quaternion> boneOffsets;
-    
-    // Start is called before the first frame update
+
     void Start()
     {
-        // ZMQ connection
+        // ZMQ connection setup
+        // Create a string URL based on the IP address and port number
         string url = "tcp://" + ip + ":" + port;
+        // Create a new publisher socket to connect to the URL
         socket = new PublisherSocket();
         socket.Connect(url); // publisher connects to subscriber
+        // Print a debug message indicating that the publisher has connected to the URL
         Debug.Log("Pub connected to: " + url + "\nSending data...");
-        
+
+        // If the Model object is not null, get its animator component and previous rotation and position
         if (Model != null)
         {
             animator = Model.GetComponent<Animator>();
             previousRotation = Model.transform.rotation;
+            previousPos = Model.transform.position;
         }
 
+        // Set the blenderInitialRotation quaternion to zero rotation
         blenderInitialRotation = new Quaternion(0, 0, 0, 1);
+        // Calculate the armature offset quaternion based on the model's current rotation
+        armatureOffset = GetOffsetQuaternion(Model.transform.rotation, new Quaternion(0.707f, 0.707f, 0f, 0f));
+        // Calculate the offset quaternion for each bone in the animator
         boneOffsets = GetAllBonesOffsetQuaternions(animator, blenderInitialRotation);
-
     }
-    
+
+
+    // Define a method that retrieves the offset quaternion for each bone in an animator
     private List<Quaternion> GetAllBonesOffsetQuaternions(Animator animator, Quaternion blenderInitialRotation)
     {
+        // Create an empty list to store the offset quaternions for each bone
         List<Quaternion> boneOffsets = new List<Quaternion>();
 
+        // Loop through each bone in the HumanBodyBones enum, except the last one (which is not valid)
         foreach (HumanBodyBones bone in Enum.GetValues(typeof(HumanBodyBones)))
         {
             if (bone == HumanBodyBones.LastBone) continue;
 
+            // Get the transform for the current bone in the animator
             Transform boneTransform = animator.GetBoneTransform(bone);
+
+            // If the bone has a valid transform, get its offset quaternion and add it to the list
             if (boneTransform != null)
             {
+                // Calculate the offset quaternion for the bone using the GetOffsetQuaternion method
                 Quaternion offsetQuaternion = GetOffsetQuaternion(boneTransform.localRotation, blenderInitialRotation);
+                // Add the offset quaternion to the boneOffsets list
                 boneOffsets.Add(offsetQuaternion);
             }
+            // If the bone does not have a valid transform, add the identity quaternion to the list
             else
             {
                 boneOffsets.Add(Quaternion.identity);
             }
         }
 
+        // Return the list of offset quaternions for each bone
         return boneOffsets;
     }
-    
-    
-    // Update is called once per frame
+
     void Update()
     {
-        if (Model.transform.rotation != previousRotation)
+        // If the model's rotation or position has changed since the last frame, send the data
+        if (Model.transform.rotation != previousRotation || Model.transform.position != previousPos)
         {
-            SendShit();
+            SendData();
+            // Update the previousRotation and previousPos variables to the current values
             previousRotation = Model.transform.rotation;
+            previousPos = Model.transform.position;
         }
-        
     }
 
-    void SendShit()
+
+    // Define a method that sends data over the ZMQ connection
+    void SendData()
     {
+        // If the "on" boolean is true, continue sending messages over the ZMQ connection
         if (on)
         {
-            // Keep sending messages until program interruption
+            // Send the model's rotation as a string using the specified topic
             byte[] topicBytes = Encoding.ASCII.GetBytes(topic);
             byte[] msgBytes = Encoding.UTF8.GetBytes(Model.transform.rotation.ToString());
-
-            // Publish data
             socket.SendMoreFrame(topicBytes).SendFrame(msgBytes);
-            Debug.Log("On topic " + topic + ", send data: " + Model.transform.rotation);
 
+            // Send the model's position as a string using a separate "transform" topic
+            byte[] positiontopicBytes = Encoding.ASCII.GetBytes("transform");
+            byte[] positionmsgBytes = Encoding.UTF8.GetBytes(Model.transform.position.ToString());
+            socket.SendMoreFrame(positiontopicBytes).SendFrame(positionmsgBytes);
 
-
+            // Loop through each bone in the HumanBodyBones enum, except the last one
             foreach (HumanBodyBones bone in Enum.GetValues(typeof(HumanBodyBones)))
             {
                 if (bone != HumanBodyBones.LastBone)
                 {
+                    // Get the index and offset quaternion for the current bone
                     int boneIndex = (int)bone;
                     Quaternion boneOffset = boneOffsets[boneIndex];
 
+                    // Initialize a new quaternion variable to store the bone's rotation with the offset applied
                     var sendmsg = new Quaternion();
+                    // Get the transform for the current bone in the animator
                     var boneTransform = animator.GetBoneTransform(bone);
+                    // If the bone has a valid transform, apply the offset quaternion to its local rotation
                     if (boneTransform != null)
                     {
                         sendmsg = ApplyOffsetQuaternion(boneOffset, boneTransform.localRotation);
-                        Debug.LogError("boneOffset: " + boneOffset + "  localRotation: " + boneTransform.localRotation +
-                                       "  sendmsg: " + sendmsg);
+                        // If the bone is the left or right upper leg, invert the Z component of the localRotation
+                        if (bone == HumanBodyBones.LeftUpperLeg || bone == HumanBodyBones.RightUpperLeg)
+                        {
+                            sendmsg.x = -sendmsg.x;
+                        }
+
+                        // Send the bone's rotation as a string using the bone's name as the topic
                         byte[] bonesTopic = Encoding.ASCII.GetBytes(bone.ToString());
                         byte[] boneTrans = Encoding.UTF8.GetBytes((sendmsg).ToString());
                         socket.SendMoreFrame(bonesTopic).SendFrame(boneTrans);
-                        Debug.Log("On topic " + bone.ToString() + ", send data Eulrt: " +
-                                  boneTransform.localRotation.ToEuler() + "Quaternion Local: " +
-                                  boneTransform.localRotation + "Quaternion rotation: " + boneTransform.rotation +
-                                  "the shit we send: " + (sendmsg).ToEuler());
                     }
                 }
             }
         }
     }
 
-
+    // Define a method that calculates the offset quaternion between two rotations
     public Quaternion GetOffsetQuaternion(Quaternion unityInitialRotation, Quaternion blenderInitialRotation)
     {
+        // Calculate the offset quaternion by multiplying the blenderInitialRotation by the inverse of the unityInitialRotation
         Quaternion offsetQuaternion = blenderInitialRotation * Quaternion.Inverse(unityInitialRotation);
         return offsetQuaternion;
     }
-    
+
+    // Define a method that applies an offset quaternion to a transform quaternion and returns the resulting quaternion
     public static Quaternion ApplyOffsetQuaternion(Quaternion transformQuaternion, Quaternion offsetQuaternion)
     {
+        // Calculate the blenderQuaternionWithOffset by multiplying the offsetQuaternion by the transformQuaternion
         Quaternion blenderQuaternionWithOffset = offsetQuaternion * transformQuaternion;
-        return blenderQuaternionWithOffset;
+        // Normalize the resulting quaternion and return it
+        return blenderQuaternionWithOffset.normalized;
     }
-    
+
+    // This method is called when the script is destroyed
     public void OnDestroy()
     {
+        // Disconnect, close, and dispose of the ZMQ socket and clean up NetMQ
         socket.Disconnect("tcp://" + ip + ":" + port);
         socket.Close();
         socket.Dispose();
         NetMQConfig.Cleanup(false);
+        // Set the "on" boolean to false
         on = false;
     }
+
 }
 
 
