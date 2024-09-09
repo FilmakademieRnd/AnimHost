c8f7e6ee66aec30ed2eb526b2e72f52a4c0bc2c0
Simon Haag <haagsimon@outlook.de>
2024-09-09 11:30:53 +0200

    Add shared style sheet to UI Utils
    - WIP

--- AnimHost/core/UIUtils.h
+++ AnimHost/core/UIUtils.h
@@ -30,6 +30,69 @@
 #include <cmath>
 
 
+class ANIMHOSTCORESHARED_EXPORT StyleSheet {
+
+	const QString mainStyleSheet = ""
+		"QHeaderView::section {"
+		"background-color:rgba(64, 64, 64, 0%);"
+		"border: 0px solid white;}"
+		
+		"QWidget{background-color:rgba(64, 64, 64, 0%);""color: white;}"
+
+		"QPushButton{"
+		"border: 1px solid white;"
+		"border-radius: 4px;"
+		"padding: 5px;"
+		"background-color:rgb(98, 139, 202);}"
+		
+		"QLabel{padding: 5px;}"
+
+		"QComboBox{"
+		"background-color:rgb(25, 25, 25);"
+		"border: 1px;"
+		"border-color: rgb(60, 60, 60);"
+		"border-radius: 4px;"
+		"padding: 5px;}"
+
+		"QComboBox::drop-down{"
+		"background-color:rgb(98, 139, 202);"
+		"subcontrol-origin: padding;"
+		"subcontrol-position: top right;"
+		"width: 15px;"
+		"border-top-right-radius: 4px;"
+		"border-bottom-right-radius: 4px;}"
+
+		"QComboBox QAbstractItemView{"
+		"background-color:rgb(25, 25, 25);"
+		"border: 1px; border-color: rgb(60, 60, 60);"
+		"border-bottom-right-radius: 4px;"
+		"border-bottom-left-radius: 4px;"
+		"padding: 0px;}"
+		
+		"QScrollBar:vertical {"
+		"border: 1px rgb(25, 25, 25);"
+		"background:rgb(25, 25, 25);"
+		"border-radius: 2px;"
+		"width:6px;"
+		"margin: 2px 0px 2px 1px;}"
+
+		"QScrollBar::handle:vertical {"
+		"border-radius: 2px;"
+		"min-height: 0px;"
+		"background-color: rgb(25, 25, 25);}"
+
+		"QScrollBar::add-line:vertical {"
+		"height: 0px;"
+		"subcontrol-position: bottom;"
+		"subcontrol-origin: margin;}"
+
+		"QScrollBar::sub-line:vertical {"
+		"height: 0px;"
+		"subcontrol-position: top;"
+		"subcontrol-origin: margin;}";
+
+};
+
 class ANIMHOSTCORESHARED_EXPORT BoneSelectionWidget : public QWidget {
 
 	Q_OBJECT
